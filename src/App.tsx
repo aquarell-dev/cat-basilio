80f5f7b3196347a5231e460d4822eceeee406c62
Ilya <aquarell.dev@gmail.com>
2023-12-04 11:19:19 +0300

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import { FC } from 'react'
-<<<<<<< HEAD
 import Layout from './components/layouts/main-layout'
-=======
->>>>>>> 696fb695e73fc3acdc0bb942fb04b03a41d2131f
 import About from './components/screens/about'
 import Contact from './components/screens/contact'
 import Projects from './components/screens/projects'
 import Skills from './components/screens/skills'
-<<<<<<< HEAD
-=======
-import Layout from './components/layouts/layout'
->>>>>>> 696fb695e73fc3acdc0bb942fb04b03a41d2131f
+
+// todo implement contact form
 
 const App: FC = () => {
 	return (
